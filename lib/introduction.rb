6932d4d9acc70fa5a1057811762db95a67fc097e
Taishawn King <taishawnking1@yahoo.com>
2019-10-23 22:51:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ def introduction_with_language(name, language)
   end
  introduction_with_language("taishawn", "ruby")
  
-def introduction_with_language_optional(name, language = "ruby")
+def introduction_with_language_optional(name, language = ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
